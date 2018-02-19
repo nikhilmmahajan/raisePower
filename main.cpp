2ac93ffb85eec85f7470b762c335882ad15c14a6
nikhilmmahajan <nikhil.mm@gmail.com>
2018-02-19 11:02:06 -0500

    raise first
    .

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
-
+//comments
 int raise(int n, int p)
 {
  if ( p < 0 ) return -1;
  if (p ==0 ) return 1;
  else
     return  n * raise(n,p-1);
+
 
 }
 
